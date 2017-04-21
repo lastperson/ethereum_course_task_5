d1aa1c8b5f1aef507a1df2391f65b508a82a359d
Alexey Shendrick <alek.code2813@gmail.com>
2017-04-21 19:40:19 +0000

    upd

--- test/debts.js
+++ test/debts.js
@@ -16,22 +16,20 @@ contract('Debts', function(accounts) {
     .then(reverter.snapshot);
   });
 
-  it('should allow to repay', () => {
-    const borrower = accounts[3];
-    const value = 1000;
-    return Promise.resolve()
-    .then(() => debts.borrow(value, {from: borrower}))
-    .then(() => debts.repay(borrower, value, {from: OWNER}))
-    .then(() => debts.debts(borrower))
-    .then(asserts.equal(0));
-  });
-
   it('should fail on overflow when borrowing', () => {
     const borrower = accounts[3];
     const value = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
     return Promise.resolve()
     .then(() => debts.borrow(value, {from: borrower}))
     .then(() => asserts.throws(debts.borrow(1, {from: borrower})));
+  });
+
+  it('should fail on underflow when repaying', () => {
+    const borrower = accounts[3];
+    const value = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
+    return Promise.resolve()
+    .then(() => debts.borrow(1, {from: borrower}))
+    .then(() => asserts.throws(debts.repay(value, {from: OWNER})));
   });
 
   it('should emit Borrowed event on borrow', () => {
@@ -47,15 +45,6 @@ contract('Debts', function(accounts) {
     });
   });
 
-  it('should allow to borrow', () => {
-    const borrower = accounts[3];
-    const value = 5000;
-    return Promise.resolve()
-    .then(() => debts.borrow(value, {from: borrower}))
-    .then(() => debts.debts(borrower))
-    .then(asserts.equal(5000));
-  });
-
   it('should emit Repayed event on repay', () => {
     const borrower = accounts[3];
     const value = 1000;
@@ -68,6 +57,23 @@ contract('Debts', function(accounts) {
       assert.equal(result.logs[0].args.by, borrower);
       assert.equal(result.logs[0].args.value.valueOf(), value);
     })
+  });
+
+  it('should allow to borrow', () => {
+    const borrower = accounts[3];
+    const value = 5000;
+    return Promise.resolve()
+    .then(() => debts.borrow(value, {from: borrower}))
+    .then(() => debts.debts(borrower))
+    .then(asserts.equal(value));
+  });
+
+  it('should allow to repay', () => {
+    const borrower = accounts[3];
+    const value = 1000;
+    return Promise.resolve()
+    .then(() => debts.borrow(value, {from: borrower}))
+    .then(() => debts.repay(borrower, value, {from: OWNER}))
     .then(() => debts.debts(borrower))
     .then(asserts.equal(0));
   });
@@ -88,14 +94,13 @@ contract('Debts', function(accounts) {
     .then(() => debts.borrow(value, {from: borrower}))
     .then(() => debts.repay(borrower, value, {from: borrower}))
     .then(() => debts.debts(borrower))
-    .then(asserts.equal(1000));
+    .then(asserts.equal(value));
   });
 
-  it('should fail on string type in amount when borrowing', () => {
-    const borrower = accounts[3];
-    const value = 'qwertyuiop';
-    return Promise.resolve()
-    .then(() => asserts.throws(debts.borrow(value, {from: borrower})));
+  it('should allow to view the owner address', () => {
+      return Promise.resolve()
+      .then(() => debts.owner())
+      .then(asserts.equal(OWNER));
   });
 
   it('does addition work?', () => {
@@ -124,60 +129,40 @@ contract('Debts', function(accounts) {
     .then(asserts.equal(0));
   });
 
-  it('does safeAdd work?', () => {
+  it('null', () => {
     const borrower = accounts[3];
-    const value = 5000;
-    const value2 = -1000;
     return Promise.resolve()
-    .then(() => debts.borrow(value, {from: borrower}))
-    .then(() => asserts.throws(debts.borrow(value2, {from: borrower})));
-  });
-
-  it('does safeSub work?', () => {
-    const borrower = accounts[3];
-    const value = 1000;
-    const value2 = 2000;
-    return Promise.resolve()
-    .then(() => debts.borrow(value, {from: borrower}))
-    .then(() => asserts.throws(debts.repay(value2, {from: OWNER})));
-  });
-
-/*it('NULL', () => {
-    const borrower = accounts[3];
-    const value = null;
-    return Promise.resolve()
-    .then(() => asserts.throws(debts.borrow(value, {from: borrower}));
-  });*/
-
-  it('should fail on ASCII when borrowing', () => {
-    const borrower = accounts[3];
-    const value = '&#32';
-    return Promise.resolve()
-    .then(() => asserts.throws(debts.borrow(value, {from: borrower})));
-  });
-
-  it('fail when try to use cyrillic word on same address', () => {
-    const borrower = "0xf95ccc102cf5bdac0b6f76f41021124a58754f4d";
-    const cyrillic = "0xf95ccc102сf5bdac0b6f76f41021124a58754f4d";
-    const value = 1000;
-    return Promise.resolve()
-    .then(() => debts.borrow(value, {from: borrower}))
-    .then(() => asserts.throws(debts.borrow(value, {from: cyrillic})))
-    .then(() => debts.repay(borrower, value, {from: OWNER}))
-    .then(() => asserts.throws(debts.borrow(cyrillic, value, {from: OWNER})))
+    .then(() => debts.borrow(null, {from: borrower}))
     .then(() => debts.debts(borrower))
     .then(asserts.equal(0));
   });
 
-  it('should fail to repay when using minus number through parseInt', () => {
+  it('should allow others to see their debts', () => {
     const borrower = accounts[3];
-    const value = parseInt("15",10);
-    const value2 = parseInt("-15",10);
+    const borrower2 = accounts[5];
+    const value = 1000;
+    const value2 = 2000;
     return Promise.resolve()
     .then(() => debts.borrow(value, {from: borrower}))
-    .then(() => asserts.throws(debts.borrow(value2, {from: borrower})))
-    .then(() => debts.debts(borrower))
-    .then(asserts.equal(15));
+    .then(() => debts.borrow(value2, {from: borrower2}))
+    .then(() => debts.debts.call(borrower, {from: borrower}))
+    .then(asserts.equal(value))
+    .then(() => debts.debts.call(borrower2, {from: borrower2}))
+    .then(asserts.equal(value2));
+  });
+
+  it('should allow owner to see others debts', () => {
+    const borrower = accounts[3];
+    const borrower2 = accounts[5];
+    const value = 1000;
+    const value2 = 2000;
+    return Promise.resolve()
+    .then(() => debts.borrow(value, {from: borrower}))
+    .then(() => debts.borrow(value2, {from: borrower2}))
+    .then(() => debts.debts.call(borrower, {from: OWNER}))
+    .then(asserts.equal(value))
+    .then(() => debts.debts.call(borrower2, {from: OWNER}))
+    .then(asserts.equal(value2));
   });
 
 });
